24f776d49c486a5e9bb689f6e36bdf5313b5cd13
Björn Henriksson <bhenriksson@gmail.com>
2020-04-07 21:20:35 +0200

    initialization
    not compiling

---
+++ SOP_developablePatcher.C
@@ -0,0 +1,121 @@
+
+
+#include "SOP_developablePatcher.h"
+
+// This is an automatically generated header file based on theDsFile, below,
+// to provide SOP_VoronoiGraphParms, an easy way to access parameter values from
+// SOP_VoronoiGraphVerb::cook with the correct type.
+#include "SOP_developablePatcher.proto.h"
+
+#include <GU/GU_Detail.h>
+#include <GEO/GEO_PrimPoly.h>
+#include <OP/OP_Operator.h>
+#include <OP/OP_OperatorTable.h>
+#include <PRM/PRM_Include.h>
+#include <PRM/PRM_TemplateBuilder.h>
+#include <UT/UT_DSOVersion.h>
+#include <UT/UT_Interrupt.h>
+#include <UT/UT_StringHolder.h>
+#include <SYS/SYS_Math.h>
+#include <limits.h>
+
+#include <OP/OP_AutoLockInputs.h>
+//#include <include/houdiniVoronoiGraph.h>
+
+using namespace HDK_Beh;
+
+/// This is the internal name of the SOP type.
+/// It isn't allowed to be the same as any other SOP's type name.
+const UT_StringHolder SOP_developablePatcher::theSOPTypeName("beh_developablePatcher"_sh);
+
+/// newSopOperator is the hook that Houdini grabs from this dll
+/// and invokes to register the SOP.  In this case, we add ourselves
+/// to the specified operator table.
+void
+newSopOperator(OP_OperatorTable *table)
+{
+    table->addOperator(new OP_Operator(
+        "beh_developablePatcher",   // Internal name
+        "Developable Patcher",                     // UI name
+		SOP_developablePatcher::myConstructor,    // How to build the SOP
+        SOP_developablePatcher::myTemplateList, // My parameters
+        1,                          // Min # of sources
+        1,                          // Max # of sources
+        nullptr,                    // Custom local variables (none)
+        NULL));        // Flag it as generator
+}
+
+/// This is a multi-line raw string specifying the parameter interface
+/// for this SOP.
+static const char *theDsFile = R"THEDSFILE(
+{
+    name        parameters
+    parm {
+        name    "divs"      // Internal parameter name
+        label   "Divisions" // Descriptive parameter name for user interface
+        type    integer
+        default { "5" }     // Default for this parameter on new nodes
+        range   { 2! 50 }   // The value is prevented from going below 2 at all.
+                            // The UI slider goes up to 50, but the value can go higher.
+        export  all         // This makes the parameter show up in the toolbox
+                            // above the viewport when it's in the node's state.
+    }
+    }
+}
+)THEDSFILE";
+
+static PRM_Name names[] = {
+	PRM_Name("divs",	"Divisions"),
+	PRM_Name("bound",	"Boundary Distance"),
+	PRM_Name("mag",	"Mag"),
+	PRM_Name("doBoundaryGeo",	"Boundary Geo"),
+	PRM_Name("resampleCurves",	"resampleCurves"),
+	PRM_Name("keepOutsideSegments",	"keepOutsideSegments"),
+	PRM_Name("removePointSegments",	"removePointSegments"),
+
+};
+PRM_Template
+SOP_developablePatcher::myTemplateList[] = {
+	PRM_Template(PRM_INT,  1, &names[0], PRMzeroDefaults),
+	PRM_Template(PRM_FLT_J,	1, &names[1], PRMzeroDefaults, 0, &PRMscaleRange),
+	PRM_Template(PRM_FLT_J,	1, &names[2], PRMzeroDefaults, 0, &PRMscaleRange),
+	PRM_Template(PRM_TOGGLE,    1, &names[3]),
+	PRM_Template(PRM_TOGGLE,    1, &names[4]),
+	PRM_Template(PRM_TOGGLE,    1, &names[5]),
+	PRM_Template(PRM_TOGGLE,    1, &names[6]),
+	PRM_Template(PRM_DIRECTION, 3, &PRMdirectionName, PRMzaxisDefaults),
+	PRM_Template(),
+};
+
+OP_ERROR
+SOP_developablePatcher::cookMySop(OP_Context &context)
+{
+	//printf("SOP_developablePatcher::cook...\n");
+	OP_AutoLockInputs inputs(this);
+	if (inputs.lock(context) >= UT_ERROR_ABORT)
+		return error();
+
+	fpreal now = context.getTime();
+
+	duplicateSource(0, context);
+	// 2. Copy input geometry into our gdp
+	// 3. Parse and create myGroup
+	if (cookInputGroups(context) >= UT_ERROR_ABORT)
+		return error();
+
+	setCurGdh(0, myGdpHandle);
+
+	//boostVoronoiGraph vor_diagram;
+	double bound = GETBOUND();
+	double mag = GETMAG();
+	double do_boundary_geo = GETDOBOUNDARYGEO();
+	bool do_resample = GETRESAMPLE();
+	bool keep_outside_segments = GETKEEPOUTIDES();
+	bool remove_point_segments = GETREMOVEPOINTSEGS();
+	//vor_diagram.addVoronoiGraphToHoudiniGeo(gdp, bound, mag, do_boundary_geo, do_resample, keep_outside_segments, remove_point_segments);
+
+	inputs.unlock();
+	return error();
+
+}
+
---
+++ SOP_developablePatcher.h
@@ -0,0 +1,50 @@
+/*
+ * This SOP builds a voronoi diagram.
+ */
+
+#ifndef __SOP_VoronoiGraph_h__
+#define __SOP_VoronoiGraph_h__
+
+#include <SOP/SOP_Node.h>
+#include <UT/UT_StringHolder.h>
+
+namespace HDK_Beh {
+/// This is the SOP class definition.  It doesn't need to be in a separate
+/// file like this.  This is just an example of a header file, in case
+/// another file needs to reference something in here.
+/// You shouldn't have to change anything in here except the name of the class.
+class SOP_developablePatcher : public SOP_Node
+{
+public:
+	static PRM_Template		 myTemplateList[];
+    static PRM_Template *buildTemplates();
+    static OP_Node *myConstructor(OP_Network *net, const char *name, OP_Operator *op)
+    {
+        return new SOP_developablePatcher(net, name, op);
+    }
+
+    static const UT_StringHolder theSOPTypeName;
+
+
+protected:
+	SOP_developablePatcher(OP_Network *net, const char *name, OP_Operator *op)
+        : SOP_Node(net, name, op)
+    {
+        mySopFlags.setManagesDataIDs(false); //manual edit, was set to false!
+    }
+	/// Method to cook geometry for the SOP
+	virtual OP_ERROR cookMySop(OP_Context &context);
+
+    virtual ~SOP_developablePatcher() {}
+
+private: 
+	double GETBOUND() { return evalFloat("bound", 0, 0); }
+	double GETMAG() { return evalFloat("mag", 0, 0); }
+	bool GETDOBOUNDARYGEO() { return evalInt("doBoundaryGeo", 0, 0) == 1 ? true : false;}
+	bool GETRESAMPLE() { return evalInt("resampleCurves", 0, 0) == 1 ? true : false;}
+	bool GETKEEPOUTIDES() { return evalInt("keepOutsideSegments", 0, 0) ==1 ? true:false; }
+	bool GETREMOVEPOINTSEGS() { return evalInt("removePointSegments", 0, 0) ==1 ? true:false; }
+};
+} // End HDK_Sample namespace
+
+#endif
